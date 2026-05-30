734077e18b7e5b90ad574f35636bc24c9263da96
Iam-AbdulSamad <mohdabdulsamad2000@gmail.com>
2026-05-30 15:54:19 +0530

    Add environment configuration and user/session models

---
+++ server/src/models/SessionModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const SessionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    refreshTokenHash: {
+        type: String,
+        required: true
+    },
+    ip: {
+        type: String,
+        required: true
+    },
+    device: {
+        type: String,
+        required: true
+    },
+    isRovked: {
+        type: Boolean,
+        default: false
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    }
+}, { timestamps: true });
+
+const Session = mongoose.model("Session", SessionSchema);
+
+module.exports = {
+    Session
+}
